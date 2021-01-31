bb2029a3ce842b34c3fde700b9c600fdaa877d85
ShamimAhamed-prog <shamim.aiub2@gmail.com>
2021-01-31 17:09:31 +0600

    second

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -27,10 +27,21 @@ const ChatFeed =(props) =>{
         });
       };
     renderMessages()
+    if (!chat) return 'Loading';
     return(
-        <div>
-           ChatFeed
+        <div className="chat-feed">
+        <div className="chat-title-container">
+          <div className="chat-title">{chat?.title}</div>
+          <div className="chat-subtitle">
+            {chat.people.map((person) => ` ${person.person.username}`)}
+          </div>
         </div>
+        {renderMessages()}
+        <div style={{ height: '100px' }} />
+        <div className="message-form-container">
+          <MessageForm {...props} chatId={activeChat} />
+        </div>
+      </div>
 
     );
 }
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,9 +1,17 @@
-const TheirMessage =() =>{
+const TheirMessage =({ lastMessage, message }) =>{
+const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+  
     return(
-        <div>
-            TheirMessage
-        </div>
+        <div className="message-row">
+        {isFirstMessageByUser && (
+          <div
+            className="message-avatar"
+            style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+          />
+    )}
+    
+    
+     </div>
     );
-}
-
+        }
 export default TheirMessage;
